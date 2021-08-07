6c5fe735d8af1ba53907c5856d62f323bc61bbbd
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2021-08-07 21:53:24 +0100

    Define Docx and send Content-Disposition header

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,29 @@
 extern crate rocket;
 use std::process::Command;
 
+struct Docx(Vec<u8>);
+
+//TODO: make async
 #[get("/<_..>/<cid>/<uid>")]
-fn hello(cid: &str, uid: &str) -> Vec<u8> {
-    Command::new("./docm-morph.py")
-        .arg("doc-samples/Anexo.docm")
-        .arg("http://example.com")
-        .output()
-        .expect("Failed to execute command").stdout
+fn hello(cid: &str, uid: &str) -> Docx {
+    Docx(
+        Command::new("./docm-morph.py")
+            .arg("doc-samples/Anexo.docm")
+            .arg("http://example.com")
+            .output()
+            .expect("Failed to execute command")
+            .stdout,
+    )
+}
+
+impl<'r> rocket::response::Responder<'r, 'static> for Docx {
+    fn respond_to(self, _: &rocket::Request<'_>) -> rocket::response::Result<'static> {
+        rocket::Response::build()
+            //TODO: make name dynamic
+            .raw_header("Content-Disposition", r#"attachment; filename="test.docm""#)
+            .sized_body(self.0.len(), std::io::Cursor::new(self.0))
+            .ok()
+    }
 }
 
 #[launch]
