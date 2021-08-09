1d94d08f1df72ffe01d70f0d6e12c0afa89a947c
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2021-08-09 10:28:12 +0100

    Make link in docs white and small

--- docm-morph.py
+++ docm-morph.py
@@ -2,10 +2,14 @@
 # patched python-docx
 # https://github.com/python-openxml/python-docx/pull/673/files
 from docx import Document
+from docx.shared import RGBColor, Pt
 import sys
 
 document = Document(sys.argv[1])
 
-document.sections[0].header.paragraphs[0].text = sys.argv[2]
+paragraph = document.sections[0].header.paragraphs[0]
+paragraph.text = sys.argv[2]
+paragraph.style.font.color.rgb = RGBColor(0xff, 0xff, 0xff)
+paragraph.style.font.size = Pt(1)
 
 document.save(sys.stdout.buffer)
